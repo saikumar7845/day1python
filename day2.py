25439a46079b53af84a73d65190942bbf6992fbd
saikumar7845 <saikumar6281577972@gmail.com>
2026-04-16 15:43:07 +0530

    day 2 variables tokens boolean comments

---
+++ day2.py
@@ -0,0 +1,16 @@
+a=90 #boolen type true or false
+b=80
+print(a==b)
+print(a!=b)
+_v=90 #define a good variable
+print(_v)
+num=9 #define a bad variable
+a,b,c=23,25,36# token 
+print(a,b,c)
+num=7 #even nuber or odd nuber
+if num%2==0:
+    print(f"{num} is even number")
+          
+else:
+    print(f"{num} is odd number")
+
