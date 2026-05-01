451470ddf26698fdc90058a9fc44df6d75e1bba4
saikumar7845 <saikumar6281577972@gmail.com>
2026-05-01 10:39:49 +0530

    if for loop

---
+++ if for loops.py
@@ -0,0 +1,31 @@
+'''for statement
+------
+--> a for statementis used to iterate bover items like (string, list,tuple) with fixed nop of itterations.
+any=[23,45,6,78]
+for j in any:
+    print(j)
+else:
+        print("loop finished")  
+else statement in for
+-------------------
+---> after completing all iterations this else statement will execute.
+any=[23,45,6,78]
+for j in any:
+    print(j)
+else:
+        print("loop finished")
+
+so="madam"
+empty=""
+for j in so:
+    empty=j+empty
+if empty== so:
+    print("palindrome")    
+else:
+    print("not a palindrome")'''
+
+
+s=20
+while s<=30:
+    print (s)
+    s+=10
